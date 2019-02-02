fb8156fa655650ac8c2bacc674df45089085abfe
krdowns <krjdowns16@gmail.com>
2019-02-01 17:22:19 -0800

    heroku trial

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/see', {useNewUrlParser: true});
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/see', {useNewUrlParser: true} );
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/see');
 
 
 
--- app/server.js
+++ app/server.js
@@ -15,13 +15,13 @@ app.use(passport.initialize());
 app.use(bodyParser.json())
 
 // HTML ENDPOINTS //
-app.get('/', (req, res) => {res.sendFile('views/index.html' , { root : __dirname});})
-app.get('/createentry', (req,res) => {res.sendFile('views/createentry.html' , { root : __dirname});})
-app.get('/emergency', (req,res) => {res.sendFile('views/emergency.html' , { root : __dirname});})
-app.get('/feed', (req, res) => {res.sendFile('views/feed.html' , { root : __dirname});})
-app.get('/history', (req,res) => {res.sendFile('views/history.html' , { root : __dirname});})
-app.get('/settings', (req, res) => {res.sendFile('views/settings.html' , { root : __dirname});})
-app.get('/signup', (req,res) => {res.sendFile('views/signup.html' , { root : __dirname});})
+app.get('./', (req, res) => {res.sendFile('views/index.html' , { root : __dirname});})
+app.get('./createentry', (req,res) => {res.sendFile('views/createentry.html' , { root : __dirname});})
+app.get('./emergency', (req,res) => {res.sendFile('views/emergency.html' , { root : __dirname});})
+app.get('./feed', (req, res) => {res.sendFile('views/feed.html' , { root : __dirname});})
+app.get('./history', (req,res) => {res.sendFile('views/history.html' , { root : __dirname});})
+app.get('./settings', (req, res) => {res.sendFile('views/settings.html' , { root : __dirname});})
+app.get('./signup', (req,res) => {res.sendFile('views/signup.html' , { root : __dirname});})
 
 app.use(express.static(__dirname + '/public'));
 
